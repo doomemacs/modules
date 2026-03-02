f57b969b583af6184fc98259835d73a5c9d9a57f
Henrik Lissner <git@henrik.io>
2026-03-02 01:38:58 -0500

    fix(debugger): remove dap-mode packages
    dap-mode and posframe were removed from this module in e0729fa, but not
    their `package!` statements. Oops.
    
    Amend: e0729fa7d7a0

--- tools/debugger/packages.el
+++ tools/debugger/packages.el
@@ -2,8 +2,3 @@
 ;;; tools/debugger/packages.el
 
 (package! dape :pin "76af39b5c274642f784823aa19bc6ea47bffdd05")
-
-;; DEPRECATED
-(when (modulep! +lsp)
-  (package! dap-mode :pin "b77d9bdb15d89e354b8a20906bebe7789e19fc9b")
-  (package! posframe :pin "41cc4def6190f100ba50dca51457c38f4f90dfb1"))
