a33db140e6c0320d38dc8c081a8e396bfa642fa5
Henrik Lissner <git@henrik.io>
2026-05-05 00:19:32 -0400

    fix(emms): match emms' :files from upstream recipe
    Amend: bd143318e6db

--- app/emms/packages.el
+++ app/emms/packages.el
@@ -2,5 +2,7 @@
 ;;; app/emms/packages.el
 
 (package! emms
-  :recipe (:host github :repo "emacs-straight/emms")
+  :recipe (:host github
+           :repo "emacs-straight/emms"
+           :files (:defaults (:exclude "doc/fdl.texi" "doc/gpl.texi")))
   :pin "d4ca43959f76bf8afc1261cdd06b2a419fe058a0")
