46b52889f4a1acd98a5645dddf5f31b18036042a
Henrik Lissner <git@henrik.io>
2026-05-12 17:47:42 -0400

    bump: dirvish
    alexluigit/dirvish@d877433f957a -> latiagertrutis/dirvish@c2406fdcc8dc
    
    Moving to a maintained fork of dirvish again, because upstream hasn't
    seen updates (and its author hasn't been active) in a year.
    
    Ref: #8732
    Ref: alexluigit/dirvish#374

--- emacs/dired/packages.el
+++ emacs/dired/packages.el
@@ -1,5 +1,7 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; emacs/dired/packages.el
 
-(package! dirvish :pin "d877433f957a363ad78b228e13a8e5215f2d6593")
+(package! dirvish
+  :recipe (:host github :repo "latiagertrutis/dirvish")
+  :pin "c2406fdcc8dccfb4f3d01f50927491eb3a156036")
 (package! diredfl :pin "fe72d2e42ee18bf6228bba9d7086de4098f18a70")
