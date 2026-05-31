5d57c5899a6d4619a53790d348d39a06adf2a803
Henrik Lissner <git@henrik.io>
2026-05-31 17:55:55 -0400

    bump: :emacs dired
    latiagertrutis/dirvish@c2406fdcc8dc -> latiagertrutis/dirvish@0238cba662f9
    
    Fix: #8732

--- emacs/dired/packages.el
+++ emacs/dired/packages.el
@@ -3,5 +3,5 @@
 
 (package! dirvish
   :recipe (:host github :repo "latiagertrutis/dirvish")
-  :pin "c2406fdcc8dccfb4f3d01f50927491eb3a156036")
+  :pin "0238cba662f977ac7766a4183c46176180909bce")
 (package! diredfl :pin "fe72d2e42ee18bf6228bba9d7086de4098f18a70")
