b5c98cbcc30cc3777a9875d8a017af971e7d10d6
Henrik Lissner <git@henrik.io>
2026-06-02 14:14:42 -0400

    fix(dashboard): show hl-line and only on current line
    I "hid" hl-line in b859727 then "disabled" it in dd1f771 by pretending
    it's already enabled. This was done because hl-line would not highlight
    the full line (sometimes only from beginning-of-text to the right window
    edge, sometimes from BOT to the BOL of the following line -- all
    depending on the method the dashboard uses to center the text, which has
    changed a couple times).
    
    Additionally, Doom uses hooks on `activate-mark-hook` and
    `deactivate-mark-hook` to temporarily disable `hl-line-mode` while any
    selection is active (because the `region` and `hl-line` faces have
    similar/the same background color in many themes). Consequently, when a
    selection is temporarily and briefly produced during mouse-click in the
    dashboard, hl-line is re-enabled (#8796). Oops.
    
    Instead of adjusting those hooks, this commit fixes the original hl-line
    issue in the dashboard. This solution only works now because the new
    dashboard module uses text-properties instead of window margins to
    center text (as of 45dfc8f). Alternatively, I could've disabled the
    :extend property on the hl-line face, but this is more vulnerable to
    breakage by themes, face-remapping packages, or user config. This fix
    does not apply (and will not be applied) to the old (and deprecated) :ui
    doom-dashboard module.
    
    Fix: #8796
    Ref: b859727f7add
    Ref: dd1f7713ea15
    Ref: 45dfc8f60cf4

--- ui/dashboard/config.el
+++ ui/dashboard/config.el
@@ -188,9 +188,9 @@ dashboard reloading is inhibited.")
   (setq-local mode-line-right-align-edge 'right-margin)
   ;; Ensure point is always on a button
   (add-hook 'post-command-hook #'+dashboard-reposition-point-h nil 'local)
-  ;; hl-line produces an ugly cut-off line highlight in the dashboard, so don't
-  ;; activate it there (by pretending it's already active).
-  (setq-local hl-line-mode t)
+  ;; hl-line will highlight up to the BOL of the following line, which looks
+  ;; ugly, so exclude the newline at EOL.
+  (setq-local hl-line-range-function (lambda () (cons (pos-bol) (pos-eol))))
   ;; Local variables are never important in the dashboard, and may cause repeat
   ;; prompts about unsafe/risky variables.
   (setq-local enable-local-variables nil))
@@ -586,7 +586,7 @@ See `+dashboard-menu-sections' to change the contents of the menu."
                      (eval when t)))
         (+dashboard-insert
          (let ((icon (if (stringp icon) icon (eval icon t))))
-           (format (format "%s%%s%%10s" (if icon "%3s\t" "%3s"))
+           (format (format " %s%%s%%10s " (if icon "%s\t" "%s"))
                    (or icon "")
                    (with-temp-buffer
                      (insert-text-button
