1e3b16a6652782233888085749fe63aa1858a38f
Henrik Lissner <git@henrik.io>
2026-06-23 01:34:41 -0400

    fix(file-templates): excessive dir traversals in short-path

--- modules/editor/file-templates/autoload.el
+++ modules/editor/file-templates/autoload.el
@@ -91,14 +91,12 @@ evil is loaded and enabled)."
 (defun +file-templates-get-short-path ()
   "Fetches a short file path for the header in Doom module templates."
   (let ((path (file-truename (or buffer-file-name default-directory))))
-    (save-match-data
-      (cond ((string-match "/modules/\\(.+\\)$" path)
-             (match-string 1 path))
-            ((file-in-directory-p path doom-emacs-dir)
-             (file-relative-name path doom-emacs-dir))
-            ((file-in-directory-p path doom-user-dir)
-             (file-relative-name path doom-user-dir))
-            ((abbreviate-file-name path))))))
+    (file-relative-name
+     path (file-truename
+           (or (locate-dominating-file path ".doommodules")
+               (locate-dominating-file path ".doom")
+               (doom-project-root)
+               default-directory)))))
 
 ;;;###autoload
 (defun +file-templates-module-for-path (&optional path)
