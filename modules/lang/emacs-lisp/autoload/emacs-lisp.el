cbcc49d432a7dc6831cc47a1512b27bfdf75a41d
Henrik Lissner <git@henrik.io>
2026-07-01 14:01:05 -0400

    fix(emacs-lisp): (void-function \,) error
    Amend: 8ee60ce0d3fd
    Fix: #25

--- modules/lang/emacs-lisp/autoload/emacs-lisp.el
+++ modules/lang/emacs-lisp/autoload/emacs-lisp.el
@@ -375,25 +375,26 @@ as `+emacs-lisp-non-package-mode' will enable it and disable the other checkers.
       flycheck-disabled-checkers)
     (setq-local flycheck-emacs-lisp-check-form
                 (prin1-to-string
-                 `(condition-case e
-                      (progn
-                        (require 'doom)
-                        (require 'doom-cli)
-                        (doom-initialize ,(doom-profile->id doom-profile))
-                        (setq doom-profile ',doom-profile
-                              doom-modules ',doom-modules
-                              doom-disabled-packages ',doom-disabled-packages
-                              byte-compile-warnings ',+emacs-lisp-linter-warnings)
-                        (doom-startup))
-                    (error
-                     (princ
-                      (format "%s:%d:%d:Error:Failed to load Doom: %s\n"
-                              (or ,(ignore-errors
-                                     (file-name-nondirectory
-                                      (buffer-file-name (buffer-base-buffer))))
-                                  (car command-line-args-left))
-                              0 0 (error-message-string e)))))
-                    ,(read (default-toplevel-value 'flycheck-emacs-lisp-check-form)))
+                 `(progn
+                    (condition-case e
+                        (progn
+                          (require 'doom)
+                          (require 'doom-cli)
+                          (doom-initialize ,(doom-profile->id doom-profile))
+                          (setq doom-profile ',doom-profile
+                                doom-modules ',doom-modules
+                                doom-disabled-packages ',doom-disabled-packages
+                                byte-compile-warnings ',+emacs-lisp-linter-warnings)
+                          (doom-startup))
+                      (error
+                       (princ
+                        (format "%s:%d:%d:Error:Failed to load Doom: %s\n"
+                                (or ,(ignore-errors
+                                       (file-name-nondirectory
+                                        (buffer-file-name (buffer-base-buffer))))
+                                    (car command-line-args-left))
+                                0 0 (error-message-string e)))))
+                    ,(read (default-toplevel-value 'flycheck-emacs-lisp-check-form))))
                 flycheck-disabled-checkers
                 (cons 'emacs-lisp-checkdoc
                       (remq 'emacs-lisp-checkdoc
