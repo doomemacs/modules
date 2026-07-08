ac0bd37eef40d93e6ce05422fc9adc16457edacf
cyl <a1008611abcd@126.com>
2026-07-08 17:57:38 +0800

    fix: correct set-tree-sitter! args, include lsp-prisma.el in recipe

--- modules/lang/prisma/config.el
+++ modules/lang/prisma/config.el
@@ -6,9 +6,11 @@
   (when (modulep! +lsp)
     (add-hook 'prisma-mode-local-vars-hook #'lsp! 'append))
   (when (modulep! +tree-sitter)
-    (set-tree-sitter! 'prisma-mode
+    (define-derived-mode prisma-ts-mode prisma-mode "Prisma"
+      "Major mode for Prisma schema files, powered by tree-sitter.")
+    (set-tree-sitter! 'prisma-mode 'prisma-ts-mode
       '((prisma :url "https://github.com/victorhqc/tree-sitter-prisma"
                 :rev "v1.6.0"))))
   (map! :localleader
         :map prisma-mode-map
-        "f" #'prisma-format-buffer))
+        "f" #'lsp-format-buffer))
