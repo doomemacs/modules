d122f8dbd4f4302f60a639bba3610dcf37f59cc4
mortebrume <me@mortebrume.eu>
2026-08-05 19:23:32 +0200

    fix(yaml): borrow yaml-mode indent for treesitter

--- modules/lang/yaml/config.el
+++ modules/lang/yaml/config.el
@@ -13,5 +13,13 @@
   :init
   (set-tree-sitter! 'yaml-mode 'yaml-ts-mode 'yaml)
   :config
+  ;; HACK: `yaml-ts-mode' does not implements any indentation,
+  ;; leaving it on `indent-relative'. Borrow `yaml-mode's
+  ;; indenter, it is installed regardless by this module.
+  ;; See upstream GNU bug report: #77094
+  (add-hook 'yaml-ts-mode-hook
+            (defun +yaml-use-yaml-mode-indentation-h ()
+              (require 'yaml-mode)
+              (setq-local indent-line-function #'yaml-indent-line)))
   (when (modulep! +lsp)
     (add-hook 'yaml-ts-mode-local-vars-hook #'lsp! 'append)))
