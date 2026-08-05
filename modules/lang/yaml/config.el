bb57e1396f1b27d98b41e1cc43734b8a53da2773
Henrik Lissner <git@henrik.io>
2026-08-05 14:14:29 -0400

    fix(yaml): use legacy indentation in yaml-ts-mode
    `yaml-ts-mode` doesn't implement semantic indentation, so we borrow the
    `yaml-mode`s.
    
    Ref: https://debbugs.gnu.org/cgi/bugreport.cgi?bug=77094
    Close: #55
    Co-authored-by: mortebrume <mortebrume@users.noreply.github.com>

--- modules/lang/yaml/config.el
+++ modules/lang/yaml/config.el
@@ -2,6 +2,12 @@
 
 (use-package! yaml-mode
   :mode "Procfile\\'"
+  :init
+  (when (modulep! +tree-sitter)
+    ;; HACK: `yaml-ts-mode' doesn't implement any indentation (falling back to
+    ;;   `indent-relative'), so borrow `yaml-mode's. See
+    ;;   https://debbugs.gnu.org/cgi/bugreport.cgi?bug=77094.
+    (setq-hook! 'yaml-ts-mode-hook indent-line-function #'yaml-indent-line))
   :config
   (when (modulep! +lsp)
     (add-hook 'yaml-mode-local-vars-hook #'lsp! 'append)))
