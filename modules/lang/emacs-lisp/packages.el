07aece519c2a7445e927ec8ec2020d718b9e48e1
Henrik Lissner <git@henrik.io>
2026-08-11 15:57:00 -0400

    bump: helpful
    Wilfred/helpful@03756fa6ad4d -> hlissner/helpful@cbaa4d267396
    
    Switch to a maintained fork.

--- modules/lang/emacs-lisp/packages.el
+++ modules/lang/emacs-lisp/packages.el
@@ -7,7 +7,9 @@
 (package! highlight-quoted :pin "24103478158cd19fbcfb4339a3f1fa1f054f1469")
 
 ;; Tools
-(package! helpful :pin "03756fa6ad4dcca5e0920622b1ee3f70abfc4e39")
+(package! helpful
+  :recipe (:host github :repo "hlissner/helpful")
+  :pin "cbaa4d2673963ecb369cf1d2f3a20247703fad02")
 (package! macrostep :pin "d0928626b4711dcf9f8f90439d23701118724199")
 (package! overseer :pin "7fdcf1a6fba6b1569a09c1666b4e51bcde266ed9")
 (package! elisp-def :pin "2451ed6594807448a24a63c51c917727713ed19d")
