5f1fdf7c90c4c89fb82bfec91d3bec96113e7bc7
Henrik Lissner <git@henrik.io>
2026-08-12 04:13:20 -0400

    tweak(lsp): re-enable lsp-ui-sideline-show-hover
    It's a feature most lsp-mode users expect, and I figure folks who choose
    it over eglot want its maximalism so the module will stick to more of
    its defaults.

--- modules/tools/lsp/+lsp.el
+++ modules/tools/lsp/+lsp.el
@@ -195,10 +195,6 @@ instead is more sensible."
         lsp-ui-doc-show-with-mouse nil  ; don't disappear on mouseover
         lsp-ui-doc-position 'at-point
         lsp-ui-sideline-ignore-duplicate t
-        ;; Don't show symbol definitions in the sideline. They are pretty noisy,
-        ;; and there is a bug preventing Flycheck errors from being shown (the
-        ;; errors flash briefly and then disappear).
-        lsp-ui-sideline-show-hover nil
         ;; Re-enable icon scaling (it's disabled by default upstream for Emacs
         ;; 26.x compatibility; see emacs-lsp/lsp-ui#573)
         lsp-ui-sideline-actions-icon lsp-ui-sideline-actions-icon-default)
