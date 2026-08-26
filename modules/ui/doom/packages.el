4054c06b535b0aa4e128344078004397a06a57e2
Henrik Lissner <git@henrik.io>
2026-08-26 10:14:29 -0400

    bump: :ui doom
    doomemacs/themes@a9b7144a4b6f -> doomemacs/themes@a59202912ad5
    
    Fix: #69

--- modules/ui/doom/packages.el
+++ modules/ui/doom/packages.el
@@ -1,5 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; ui/doom/packages.el
 
-(package! doom-themes :pin "a9b7144a4b6fe03be144b1610e0a6840e2b24d41")
+(package! doom-themes :pin "a59202912ad55014e53a685eee6cd94130bdd4fd")
 (package! solaire-mode :pin "1bd0134194e48c8fe4089e9d505517935b2b15e3")
